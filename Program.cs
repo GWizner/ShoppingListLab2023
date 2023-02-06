835bd43fc44ac6afe3ad9a402c742fa261bb7608
Gregory Wizner <wiznergregory@gmail.com>
2023-02-06 18:31:12 -0500

    Updated cart

--- Program.cs
+++ Program.cs
@@ -1,160 +1,169 @@
-﻿
-
-using System.Diagnostics.Metrics;
+﻿using System;
 using System.Globalization;
-using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
 
-Dictionary<string, decimal> shoppingList = new Dictionary<string, decimal>();
-shoppingList["Resident Evil IV"] = 59.99m;
-shoppingList["Final Fantasy VII Remake"] = 59.99m;
-shoppingList["Octopath Traveler II"] = 59.99m;
-shoppingList["The Callisto Protocol"] = 59.99m;
-shoppingList["Ghost of Tsushima"] = 59.99m;
-shoppingList["God of War Ragnarok"] = 59.99m;
-shoppingList["Sonic Frontiers"] = 59.99m;
-shoppingList["Call of Duty: Modern Warfare II Cross-Gen Edition"] = 59.99m;
-
-var byTitle = shoppingList.OrderBy(x => x.Key.ToLower().Replace("the ", ""))
-                              .ToDictionary(x => x.Key, x => x.Value);
-List<string> keyCart = new();
-List<decimal> valueCart = new();
-
-
-
-Console.WriteLine("Welcome to our game store. Here are our hot ticket items: ");
-Console.WriteLine();
-
-decimal total = 0;
-int counter = 0;
-int index = 0;
-bool menu = true;
-
-
-while (menu)
+namespace ShoppingListLab2023
 {
-    foreach (var pair in byTitle)
-    {
-        if (counter == 0)
-        {
-            Thread.Sleep(500);
-        }
-        else
-        {
-            Thread.Sleep(250);
-        }
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write(String.Format("{0, -50}", pair.Key));
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(String.Format("{0, 10}", pair.Value));
-        Console.ResetColor();
-
-    }    
-    
-    bool noMenu = true;
-    while (noMenu)
+    class Program
     {
 
-        Console.WriteLine("\nPlease select which game you would like to purchase: ");
-        string userInput1 = Console.ReadLine().ToLower();
-
-        string selectedKey = byTitle.Keys.FirstOrDefault(x => x.Equals(userInput1, StringComparison.OrdinalIgnoreCase));
-        if (selectedKey != null)
+        public static void Main(string[] args)
         {
-            counter++;
 
-            foreach (var game in byTitle)
+            Dictionary<string, decimal> shoppingList = new Dictionary<string, decimal>();
+            shoppingList["Resident Evil IV"] = 59.99m;
+            shoppingList["Final Fantasy VII Remake"] = 59.99m;
+            shoppingList["Octopath Traveler II"] = 59.99m;
+            shoppingList["The Callisto Protocol"] = 59.99m;
+            shoppingList["Ghost of Tsushima"] = 59.99m;
+            shoppingList["God of War Ragnarok"] = 59.99m;
+            shoppingList["Sonic Frontiers"] = 59.99m;
+            shoppingList["Call of Duty: Modern Warfare II Cross-Gen Edition"] = 59.99m;
+
+            var byTitle = shoppingList.OrderBy(x => x.Key.ToLower().Replace("the ", ""))
+                                          .ToDictionary(x => x.Key, x => x.Value);
+
+            List<Cart> cart = new List<Cart>();
+           
+            Console.WriteLine("Welcome to our game store. Here are our hot ticket items: ");
+            Console.WriteLine();
+
+            decimal total = 0;
+            int counter = 0;
+            int index = 0;
+            bool menu = true;
+            bool cashOut = false;
+
+
+            while (menu)
             {
-                if (game.Key.Equals(selectedKey, StringComparison.OrdinalIgnoreCase))
+                foreach (var pair in byTitle)
                 {
-                    keyCart.Add(selectedKey);
-                    valueCart.Add(byTitle[selectedKey]);
-                    total = total + byTitle[selectedKey];
+                    //if (counter == 0)
+                    //{
+                    //    Thread.Sleep(500);
+                    //}
+                    //else
+                    //{
+                    //    Thread.Sleep(250);
+                    //}
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(String.Format("{0, -50}", pair.Key));
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(String.Format("{0, 10:C}", pair.Value));
+                    Console.ResetColor();
+
                 }
-            }
 
-            Console.Write("\nOkay, I'll add that to your cart. ");
-
-            bool purchaseLoop = true;
-            while (purchaseLoop)
-            {
-                Console.WriteLine("Would you like to purchase another game (y/n)?\n");
-                string yesNo = Console.ReadLine().ToLower();
-
-                while (true)
+                bool noMenu = true;
+                while (noMenu)
                 {
-                    if (yesNo == "y")
-                    {
-                        Console.WriteLine("\nIf you would like to see the menu again, please input '1': ");
-                        string userInput2 = Console.ReadLine();
-                        menu = int.TryParse(userInput2, out int userChoice);
 
-                        if (String.IsNullOrWhiteSpace(userInput2))
+                    Console.Write("\nPlease select which game you would like to purchase ");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write($"(Check your spelling!)");
+                    Console.ResetColor();
+                    Console.WriteLine(". To view your cart, input '2': ");
+                    string userInput1 = Console.ReadLine().ToLower();
+
+                    string selectedKey = byTitle.Keys.FirstOrDefault(x => x.Equals(userInput1, StringComparison.OrdinalIgnoreCase));
+                    if (selectedKey != null)
+                    {
+                        counter++;
+
+                        foreach (var game in byTitle)
                         {
-                            menu = false;
+                            if (game.Key.Equals(selectedKey, StringComparison.OrdinalIgnoreCase))
+                            {
+                                cart.Add(new Cart(selectedKey, byTitle[selectedKey]));
+                                total = total + byTitle[selectedKey];
+                            }
                         }
-                        else if (userChoice == 1)
+
+                        Console.Write("\nOkay, I'll add that to your cart. ");
+
+                        bool purchaseLoop = true;
+                        while (purchaseLoop)
                         {
-                            Console.WriteLine();
-                            noMenu = false;
-                            menu = true;
+                            Console.WriteLine("Would you like to purchase another game (y/n)?\n");
+                            string yesNo = Console.ReadLine().ToLower();
+
+                            while (true)
+                            {
+                                if (yesNo == "y")
+                                {
+                                    Console.WriteLine("\nIf you would like to see the menu again, please input '1'. If you " +
+                                        "would like to view your cart input '2': ");
+                                    string userInput2 = Console.ReadLine();
+                                    menu = int.TryParse(userInput2, out int userChoice);
+
+                                    if (String.IsNullOrWhiteSpace(userInput2))
+                                    {
+                                        menu = false;
+                                    }
+                                    else if (userChoice == 1)
+                                    {
+                                        Console.WriteLine();
+                                        noMenu = false;
+                                        menu = true;
+                                    }
+                                    else if (userChoice == 2)
+                                    {
+                                        cashOut = false;
+                                        ViewCart.currentCart(cart, total, cashOut);
+                                    }
+                                    else
+                                    {
+                                        menu = false;
+                                    }
+                                    purchaseLoop = false;
+                                    break;
+                                }
+                                else if (yesNo == "n")
+                                {
+                                    Console.WriteLine("\nSo you've got: \n");
+                                    cashOut = true;
+                                    ViewCart.currentCart(cart, total, cashOut);
+
+                                    noMenu = false;
+                                    menu = false;
+                                    purchaseLoop = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nNrrrp! Try again!\n");
+                                    purchaseLoop = true;
+                                    break;
+                                }
+                            }
                         }
-                        else
-                        {
-                            menu = false;
-                        }
-                        purchaseLoop = false;
-                        break;
                     }
-                    else if (yesNo == "n")
+                    else if (userInput1 == "2")
                     {
-                        Console.WriteLine("\nSo you've got: \n");
-
-                        for (int i = 0; i < keyCart.Count; i++)
-                        {
-                            Thread.Sleep(500);
-                            CultureInfo cultureInfo = CultureInfo.CurrentCulture;
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write(String.Format("{0, -50}", cultureInfo.TextInfo.ToTitleCase(keyCart[i])));
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(String.Format("{0, 10}", valueCart[i]));
-                            Console.ResetColor();
-                        }
-                        Thread.Sleep(800);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("--------------------------------------------------" +
-                            "----------");
-                        Console.Write(String.Format("{0, -50}", "Total: "));
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine(String.Format("{0, 10}", total));
-                        Console.ResetColor();
-
-                        noMenu = false;
-                        menu = false;
-                        purchaseLoop = false;
-                        break;
+                        Console.WriteLine();
+                        ViewCart.currentCart(cart, total, cashOut);
                     }
                     else
                     {
-                        Console.WriteLine("\nNrrrp! Try again!\n");
-                        purchaseLoop = true;
-                        break;
+                        Console.Write($"\nNrrrp! \"{userInput1}\" don't exist! ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"(Check your spelling!)");
+                        Console.ResetColor();
                     }
                 }
             }
-        }
-        else
-        {
-            Console.Write($"\nNrrrp! \"{userInput1}\" don't exist! ");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"(Check your spelling!)");
-            Console.ResetColor();
+            Console.WriteLine("\n\nGoodbye!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Thread.Sleep(700);
         }
     }
 }
-Console.WriteLine("\n\nGoodbye!");
-Console.WriteLine();
-Console.WriteLine();
-Thread.Sleep(700);
 
 
 
